5d66043a94541b29be5309964b486a53aa653508
Aza Gutierrez <azabel10@gmail.com>
2017-12-26 17:26:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+function happyHolidaysTo(name) {
+  return "Happy holidays, you!"
+}
+funtion happyHolidaysTo(holiday, name) {
+  return "Thanksgiving, Joe!"
+}
